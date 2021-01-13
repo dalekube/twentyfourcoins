704c9c0645e65db0984afbb8b918bfeeec36be1d
Dale Kube <dkube@uwalumni.com>
2021-01-12 22:48:54 -0600

    commit to RF

---
+++ archive/predict-price-tensorflow.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Train the model for BAT-USDC
+
+Example call: python3 predict-price.py BAT-USDC weights-BAT-USDC-0.0005.hdf5
+
+    Parameters:
+        Coin (string): The code for a coin (e.g. BAT-USDC)
+        Model Object (string): Filename for the model object to use for the inference
+
+@author: dale
+"""
+
+import os
+import sys
+import json
+import re
+
+import numpy as np
+import pandas as pd
+from datetime import datetime
+import pickle
+
+#import tensorflow as tf
+from tensorflow import keras
+
+## Check if the GPU is available
+#print("[INFO] GPU Configuration =", tf.config.list_physical_devices('GPU'))
+#print("[INFO] TensorFlow version =", tf.__version__)
+
+## DEVELOPMENT ONLY
+## os.chdir('/home/dale/Downloads/GitHub/coinML')
+
+# Load the configurations
+with open('config.json') as f:
+    config = json.load(f)
+
+# Evaluate the command line parameters
+# argv[1] = coin name (e.g. BAT-USDC)
+# argv[2] = model object name (e.g. weights-BAT-USDC-0.0022.hdf5)
+if len(sys.argv) != 3:
+    
+    # Error: missing command line arguments
+    print("[ERROR] Missing command line arguments.")
+    
+else:
+    
+    ## DEVELOPMENT ONLY
+    ## COIN = 'BAT-USDC'
+    
+    COIN = sys.argv[1]
+    assert COIN in config['SUPPORTED_COINS'], "[ERROR] " + COIN + " is not supported"
+    DATA_DIR = './data/'
+    
+    # Load the data for the coin
+    # Print the row count when finished
+    COIN_CSV = DATA_DIR + '/' + COIN + '.csv'
+    assert COIN in config['SUPPORTED_COINS'], "[ERROR] " + COIN + " is not supported"
+    assert os.path.exists(COIN_CSV), "[ERROR] Unavailable data (.csv) for " + COIN
+    df = pd.read_csv(COIN_CSV, low_memory=False)
+    N_DF = len(df)
+    assert N_DF > 0, "[ERROR] Zero rows in the data file (.csv) for " + COIN
+    print("[INFO] Successfully read", '{:,}'.format(N_DF), "records from file")
+    
+    # Calculate rolling average features
+    df.sort_values(by=['time'], inplace=True)
+    for i in range(10, 2000, 10):
+        df['MA'+str(i)] = df['close'].rolling(window=i).mean()
+    
+    # Drop rows with na values and convert to float32
+    df.fillna(0, inplace=True)
+    df = df.astype(np.float32)
+    
+    ## DEVELOPMENT ONLY
+    ## MODEL_PATH = './models/BAT-USDC/weights-BAT-USDC-0.0003.hdf5'
+    ## MODEL_PATH = './models/' + COIN + '/RF-0.0001.pkl'
+    
+    # Load the neural network model
+    assert sys.argv[2] is not None, "[ERROR] Null model file path"
+    MODEL_PATH = './models/' + COIN + '/' + sys.argv[2]
+    if re.match(".*\/weights.*hdf5$", MODEL_PATH):
+        print("[INFO] Loading the TensorFlow model", MODEL_PATH)
+        model = keras.models.load_model(MODEL_PATH)
+    elif re.match(".*\/RF.*pkl$", MODEL_PATH):
+        print("[INFO] Loading the RangerForestRegressor model", MODEL_PATH)
+        with open(MODEL_PATH, 'rb') as f:
+            model = pickle.load(f)
+    else:
+        print("[ERROR] Model path does not align to a valid model")
+        sys.exit(0)
+    
+    # Make prediction with the latest observation closest to NOW()
+    df['UTC_TIME'] = df.apply(lambda row: datetime.utcfromtimestamp(row['time']), axis=1)
+    df.sort_values(by=['time'], inplace=True)
+    df_predict = df.tail(1)
+    p_time = df_predict.pop('UTC_TIME')
+    p_time = str(p_time.tolist()[0])
+    p_close = float(df_predict['close'])
+    p_now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    
+    print("[INFO] Current time = " + p_now)
+    print("[INFO] Making forward-looking prediction from " + p_time)
+    prediction = float(model.predict(df_predict))
+    
+    print("[INFO] Predicted price = " + str(prediction))
+    EXPECTED_CHANGE = prediction-p_close
+    print("[RESULT] The price is expected to change by " + str(round(EXPECTED_CHANGE,3)) + " dollars in the next 24 hours")
---
+++ archive/train-model-tensorflow.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Train the model for the specified coin.
+
+Example call: python3 train-model.py BAT-USDC
+
+    Parameters:
+        Coin (string): The code for a coin (e.g. BAT-USDC)
+
+@author: Dale Kube (dkube@uwalumni.com)
+"""
+
+import os
+import sys
+import json
+
+import pandas as pd
+import numpy as np
+
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Check if the GPU is available
+print("[INFO] GPU Configuration =", tf.config.list_physical_devices('GPU'))
+print("[INFO] TensorFlow version =", tf.__version__)
+
+## DEVELOPMENT ONLY
+## os.chdir('/home/dale/Downloads/GitHub/coinML')
+
+# Load the configurations
+with open('config.json') as f:
+    config = json.load(f)
+
+# Evaluate the command line parameters
+# argv[1] = coin name (e.g. BAT-USDC)
+if len(sys.argv) > 1:
+
+    COIN = sys.argv[1]
+    
+    ## DEVELOPMENT ONLY
+    ## COIN = 'BAT-USDC'
+    
+    # Create the coin directory if necessary
+    DATA_DIR = './data/'
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+    
+    # Load the data for the coin
+    # Print the row count when finished
+    COIN_CSV = DATA_DIR + COIN + '.csv'
+    assert COIN in config['SUPPORTED_COINS'], "[ERROR] " + COIN + " is not supported"
+    assert os.path.exists(COIN_CSV), "[ERROR] Unavailable data (.csv) for " + COIN
+    df = pd.read_csv(COIN_CSV, low_memory=False)
+    N_DF = len(df)
+    assert N_DF > 0, "[ERROR] Zero rows in the data file (.csv) for " + COIN
+    print("[INFO] Successfully read", '{:,}'.format(N_DF), "records from file")
+    
+    # Calculate rolling average features
+    df.sort_values(by=['time'], inplace=True)
+    for i in range(10, 2000, 10):
+        df['MA'+str(i)] = df['close'].rolling(window=i).mean()
+    
+    # Drop rows with na values and convert to float32
+    df.fillna(0, inplace=True)
+    df = df.astype(np.float32)
+    
+    # Sort and calculate the 24 hours forward closing price (288 time periods of five minutes)
+    # 86400/300 = 288
+    # Remove observations without a 24-hour price target
+    df.sort_values(by=['time'], inplace=True)
+    df['PRICE_24'] = df['close'].shift(288)
+    df = df[~pd.isnull(df['PRICE_24'])]
+    
+    # Split the training and testing data
+    # Use the latest 1,000 observations for the test split
+    price_24 = df.pop('PRICE_24')
+    N_TEST = 1000
+    N_DF = len(df)
+    
+    x_train = df[0:N_DF-N_TEST]
+    y_train = price_24[0:N_DF-N_TEST]
+    
+    x_test = df[-N_TEST:]
+    y_test = price_24[-N_TEST:]
+    
+    # Model architecture and layers
+    model = keras.Sequential()
+    model.add(keras.Input(shape=(df.shape[1],)))
+    model.add(layers.Dense(300, kernel_initializer='normal', activation='relu'))
+    model.add(layers.Dense(300, kernel_initializer='normal', activation='relu'))
+    model.add(layers.Dense(1, kernel_initializer='normal'))
+    
+    # Compile and summarize the model
+    model.compile(optimizer=keras.optimizers.Adam(lr=10e-3), loss='mean_squared_error')
+    model.summary()
+    
+    # Model checkpoint
+    # Iteratively save the model weights after improvement
+    MODEL_DIR = './models/' + COIN + '/'
+    MODEL_FILE = MODEL_DIR + 'weights-' + COIN + '-{val_loss:.4f}.hdf5'
+    ModelCheck = keras.callbacks.ModelCheckpoint(MODEL_FILE, monitor='val_loss',\
+    verbose=1, save_weights_only=False, mode='min', save_best_only=True)
+    
+    # Early stopping to avoid unnecessary computation and save time
+    EarlyStop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5,\
+    verbose=1, restore_best_weights=True)
+    
+    # Compile the callbacks into a single list
+    callbacks = [ModelCheck, EarlyStop]
+    
+    history = model.fit(x_train, y_train, epochs=2, verbose=1,
+              callbacks=callbacks, validation_data=(x_test, y_test), shuffle=True)
+    
+    # Visualize the model performance over the epochs
+    # Exclude the first five epochs
+    hist = history.history
+    hist = {k: hist[k][5:] for k in hist}
+    plt.plot(hist['loss'])
+    plt.plot(hist['val_loss'])
+    plt.title('model loss (MSE)')
+    plt.ylabel('MSE')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.show()
+    
+    # Save the training history
+    HISTORY_FILE = './models/' + COIN + '/history-' + COIN + '.json'
+    json.dump(hist, open(HISTORY_FILE, 'w'))
+    
+    # Finished
+    print("[FINISHED] Successfully trained the model for", COIN)
+
+else:
+    
+    # Error: missing command line arguments
+    print("[ERROR] Missing command line arguments. Must specify coin name (e.g. BAT-USDC)")
+
