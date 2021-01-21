a77f60710885393c159e8bf10bfdcf9276670231
Dale Kube <dkube@uwalumni.com>
2021-01-20 19:37:41 -0600

    port 80 hosting in aws

--- wsgi.py
+++ wsgi.py
@@ -11,4 +11,4 @@ from twentyfourcoins import app
 if __name__ == '__main__':
     context = ('cert.pem', 'key.pem')
     #app.run(debug=True, ssl_context=context)
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
